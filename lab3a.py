435be85e1903c12ecc7bc4a504e3139c61338f20
mzhossain5 <sonyaks401@gmail.com>
2024-06-11 07:22:28 -0900

    Initial commit with lab files

---
+++ lab3a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: [134746221]
+
+def return_text_value():
+    return "Good Morning Terry"
+
+# return_number_value() function
+
+def return_number_value():
+    value1 = 7
+    value2 = 8
+    return value1 + value2
+
+# Main Program
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
